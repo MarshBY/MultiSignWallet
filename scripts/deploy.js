5920045819fcf7f534f4ceb5e35ed2035654a2ce
Marcel <marshby@gmail.com>
2022-06-14 18:30:17 +0200

    Set network to rinkeby with deployed contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,11 @@
 async function main() {
+
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
     // We get the contract to deploy
     const Factory = await ethers.getContractFactory("MultiSignWalletFactory");
     const factory = await Factory.deploy();
