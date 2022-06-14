99ad08bc7c176455e1550e3e12af0c97b0b091fb
Marcel <marshby@gmail.com>
2022-06-14 19:31:56 +0200

    PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('public'))
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT);
